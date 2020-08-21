5df29f81f76b5f0465e785187d304f69cf764988
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2020-08-21 11:14:39 +0300

    cleanup

--- commands/check.sh
+++ commands/check.sh
@@ -7,4 +7,4 @@ docker exec backend ./src/manage.py validate_templates &&
 docker-compose -f docker-compose.yml config --quiet &&
 docker exec nginx nginx -t &&
 docker exec -it backend flake8 ./src &&
-docker exec -it backend pytest ./src/tests -s -vv -x --create-db
+docker exec -it backend pytest ./src/tests -s -vv -x --create-db --cov-fail-under=15
